72f2055af2324b94f2d204a4289e27a3ea3fb9e0
Mario Meko <marioo.meko@outlook.com>
2022-09-27 18:09:10 +0200

    initial commit

---
+++ src/coffees/coffees.service.ts
@@ -0,0 +1,50 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Coffee } from './entities/coffee.entity';
+
+@Injectable()
+export class CoffeesService {
+    private coffees : Coffee[]= [
+        {id: 1,
+        name : "Mulliri i vjeter",
+        brand: "Greke",
+        flavors : ['chocolate', 'vanilla'],
+         },
+        {
+        id: 2,
+        name : "MonCheri",
+        brand: "French",
+        flavors : ['caramel'],
+        }
+    ];
+
+    findAll(){
+        return this.coffees;
+    }
+
+    findOne(id:string){
+        const coffee = this.coffees.find(item => item.id === +id);
+        if(!coffee){
+            throw new HttpException(`Coffee #${id} not found`, HttpStatus.NOT_FOUND);
+        }
+        return coffee;
+    }
+
+    create(createCoffeeDto:any){
+        this.coffees.push(createCoffeeDto);
+    }
+    
+    update(id: string, updateCoffeeDto : any){
+        const coffee =  this.findOne(id);
+        if(coffee){
+//update COFFFEE
+        }
+        
+    }
+    
+    remove(id:string){
+        const coffeeIndex = this.coffees.findIndex(item => item.id === +id);
+        if (coffeeIndex>= 0){
+            this.coffees.splice(coffeeIndex,1);
+        }
+    }
+}
